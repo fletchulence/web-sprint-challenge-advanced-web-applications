72e5e93e0ef1bf9742a0a31aadca051e4503fe72
David Fletcher <davidfletcher@MBP14inch2021-1301.lan>
2021-11-13 20:34:07 -0800

    trying my hand at the articleSrevices one last time

--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,6 +1,27 @@
+import axiosWithAuth from "../utils/axiosWithAuth"
 
+const articleService = () => {
+   return axiosWithAuth()
+      .get('http://localhost:5004/api/articles')
+      .then(res=>{
+         const { data } = res.data
 
-const articleService = ()=> {
+      return {
+         articles:[{
+            article:{
+               author: data.author,
+               createdoOn: data.createdOn, //timestamp of when article was added
+               id: data.id, //unique article id
+               headline: data.headline, //title of article
+               summary: data.summart, //short summary statement of article
+               body: data.body  //paragraph of article text
+            }
+         }]
+      }
+
+         })
+         .catch(err=>console.error(err))
+
 }
 
 export default articleService;
